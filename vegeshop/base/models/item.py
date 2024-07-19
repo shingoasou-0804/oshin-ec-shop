2949766394e3f6e70fc66c8cad729b4755ed95ba
shingoasou-0804 <122670383+shingoasou-0804@users.noreply.github.com>
2024-07-20 06:44:58 +0900

    add item model

---
+++ vegeshop/base/models/item.py
@@ -0,0 +1,38 @@
+import os
+
+from django.db import models
+from django.utils.crypto import get_random_string
+
+
+def create_id():
+    return get_random_string(22)
+
+
+def upload_image(instance, filename):
+    item_id = instance.id
+    return os.path.join("static", "items", item_id, filename)
+
+
+class Item(models.Model):
+    id = models.CharField(
+        default=create_id,
+        primary_key=True,
+        max_length=22,
+        editable=False
+    )
+    name = models.CharField(default="", max_length=50)
+    price = models.PositiveIntegerField(default=0)
+    stock = models.PositiveIntegerField(default=0)
+    description = models.TextField(default="", blank=True)
+    sold_count = models.PositiveIntegerField(default=0)
+    is_published = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    image = models.ImageField(
+        default="",
+        blank=True,
+        upload_to=upload_image
+    )
+
+    def __str__(self) -> str:
+        return self.name
